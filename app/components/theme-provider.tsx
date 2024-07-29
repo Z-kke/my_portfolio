540d98c2c7d6a49b4ca53e49e7b563b6064809f5
Saku <sakurho@gmail.com>
2024-07-29 16:43:19 +0300

    Modify the theme a tad.

--- app/components/theme-provider.tsx
+++ app/components/theme-provider.tsx
@@ -7,12 +7,31 @@ import CssBaseline from "@mui/material/CssBaseline";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#1976d2",
+      main: "#2563eb",
     },
     secondary: {
-      main: "#dc004e",
+      main: "#6c757d",
+    },
+    background: {
+      default: "#0a192f",
+    },
+    text: {
+      primary: "#8892b0",
     },
   },
+  typography: {
+    fontFamily: "sans-serif",
+    h1: {
+      fontFamily: "monospace",
+      fontWeight: 300,
+      fontSize: "1.2rem",
+      color: "#2563eb",
+    },
+    h2: {
+      color: "#ccd6f6",
+    },
+  },
+  spacing: 8,
 });
 
 export default function MuiThemeProvider({
