bebd66c69e8500eadc78c09b7cbfcd12f69128fc
Saku Arho <sakurho@gmail.com>
2024-08-04 17:09:38 +0300

    Add the new components to the list to be rendered.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { Typography } from "@mui/material";
 import ResponsiveAppBar from "./components/ResponsiveAppBar";
 import Introduction from "./components/Introduction";
 import About from "./components/AboutMe";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 export default function Home() {
   const appbarItems = ["About", "Experience", "Work", "Contact"];
@@ -19,7 +21,12 @@ export default function Home() {
       height: 1,
     },
     {
-      component: Introduction,
+      component: Work,
+      width: 12,
+      height: 1,
+    },
+    {
+      component: Contact,
       width: 12,
       height: 1,
     },
