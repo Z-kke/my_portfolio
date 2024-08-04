43aab605845f374477e476d1f9997889df2a4ebc
Saku Arho <sakurho@gmail.com>
2024-08-04 17:53:12 +0300

    Remove the footer.

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,15 +21,6 @@ const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
             <Box component="main" sx={{ flexGrow: 1 }}>
               {children}
             </Box>
-            <Box component="footer" sx={{ py: 2, mt: 8 }}>
-              <Grid container justifyContent="center">
-                <Grid item>
-                  <Typography variant="h1">
-                    © {new Date().getFullYear()} Saku Arho
-                  </Typography>
-                </Grid>
-              </Grid>
-            </Box>
           </Box>
         </MuiThemeProvider>
       </body>
