71157553fb19beddd5f42743a58cec334e2e1370
Saku Arho <sakurho@gmail.com>
2024-08-04 18:16:25 +0300

    Tidy up the page.tx

--- app/page.tsx
+++ app/page.tsx
@@ -2,35 +2,14 @@ import DynamicGrid from "./components/DynamicGrid";
 import ResponsiveAppBar from "./components/ResponsiveAppBar";
 
 export default function Home() {
-  const appbarItems = ["About", "Experience", "Work", "Contact"];
+  const items = ["Introduction", "AboutMe", "Work", "Contact"];
 
-  const items = [
-    {
-      component: "Introduction",
-      width: 12,
-      height: 1,
-    },
-    {
-      component: "AboutMe",
-      width: 12,
-      height: 1,
-    },
-    {
-      component: "Work",
-      width: 12,
-      height: 1,
-    },
-    {
-      component: "Contact",
-      width: 12,
-      height: 1,
-    },
-  ];
+  const components = items.map((item) => ({ component: item }));
 
   return (
     <div>
-      <ResponsiveAppBar pages={appbarItems} />
-      <DynamicGrid items={items} />
+      <ResponsiveAppBar pages={items} />
+      <DynamicGrid items={components} />
     </div>
   );
 }
