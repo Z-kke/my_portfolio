9f2e0d2dd83e9f446c042b04d7be0816111a7763
Saku Arho <sakurho@gmail.com>
2024-08-17 11:51:18 +0300

    Add grid to make component more responsive and also add a fixed width so the Typewriter doesn't modify components width at runtime.

--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -1,50 +1,60 @@
-import { Typography, Box, Slide } from "@mui/material";
+import { Typography, Box, Slide, Grid } from "@mui/material";
 import Typewriter from "typewriter-effect";
 
 export default function Introduction() {
   return (
     <Slide direction="up" in={true} timeout={1000}>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "start",
-          justifyContent: "start",
-          textAlign: "center",
-          my: 10, // TODO: move this styling to the parent.
-        }}
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        sx={{ minHeight: "50vh" }}
       >
-        <Typography variant="h1" gutterBottom>
-          Hi, my name is
-        </Typography>
-        <Typography my={0} variant="h3" gutterBottom>
-          Saku Arho
-        </Typography>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            fontFamily: "monospace",
-            color: "#FF6B3D",
-            my: 2,
-            padding: 1,
-            border: 1,
-            borderRadius: 3,
-          }}
-        >
-          I am&nbsp;
-          <Typewriter
-            options={{
-              strings: ["a software engineer.", "a full-stack developer."],
-              autoStart: true,
-              loop: true,
-              delay: 50,
-              deleteSpeed: 30,
+        <Grid item xs={12} sm={10} md={8} lg={6} xl={4}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              justifyContent: "center",
+              textAlign: "left",
             }}
-          />
-        </Box>
-      </Box>
+          >
+            <Typography variant="h1" gutterBottom>
+              Hi, my name is
+            </Typography>
+            <Typography my={0} variant="h3" gutterBottom>
+              Saku Arho
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                fontFamily: "monospace",
+                color: "#FF6B3D",
+                my: 2,
+                padding: 1,
+                border: 1,
+                borderRadius: 3,
+                width: "fit-content",
+                maxWidth: "100%",
+              }}
+            >
+              I am&nbsp;
+              <Typewriter
+                options={{
+                  strings: ["a software engineer.", "a full-stack developer."],
+                  autoStart: true,
+                  loop: true,
+                  delay: 50,
+                  deleteSpeed: 30,
+                }}
+              />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
     </Slide>
   );
 }
